eebb1b03cf249280e255d3e92d889952c1d9c76d
edm1001 <edmer_franciz@yahoo.com>
2023-08-19 16:14:58 -0500

    apply crud operations for voter

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host : 'localhost',
         user: 'root',
-        password: '1861',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
